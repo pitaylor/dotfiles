5b30324d70160cb32ec2fa2a6178a698288e33a6
Pete Taylor <peter.taylor@gmail.com>
2017-03-11 16:00:25 -0600

    Basic dotfiles

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+make_links() {
+  if [ -d "${1}" ]; then
+    cp -fvrs "${1}/." ~
+  fi
+}
+
+# On OSX have coreutils preempt default commands
+PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+
+platform=$(uname)
+base_dir=$(readlink --canonicalize "$(dirname "${0}")")
+
+ln -fns "${base_dir}" ~/.dotfiles
+make_links "${base_dir}/default"
+make_links "${base_dir}/${platform}"
