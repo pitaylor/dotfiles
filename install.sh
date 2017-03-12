ea5f5002caad6233bd3c9ad92e78ae4cb6855d5b
Peter Taylor <peter.taylor@gmail.com>
2017-03-11 22:49:22 -0600

    Hook into profile script

--- install.sh
+++ install.sh
@@ -2,7 +2,13 @@
 
 make_links() {
   if [ -d "${1}" ]; then
-    cp -fvrs "${1}/." ~
+    cp -fvrs "${1}/." "${HOME}"
+  fi
+}
+
+install_profile() {
+  if ! grep -q '$HOME/.dotfiles/profile.sh' "${1}"; then
+    echo '[ -s "$HOME/.dotfiles/profile.sh" ] && source "$HOME/.dotfiles/profile.sh"' >> "${1}"
   fi
 }
 
@@ -12,5 +18,13 @@ PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
 platform=$(uname)
 base_dir=$(readlink --canonicalize "$(dirname "${0}")")
 
+# Symlink dot files to home directory
 make_links "${base_dir}/default"
 make_links "${base_dir}/${platform}"
+
+# Load profile.sh from .bash_profile or .profile
+if [ -s "${HOME}/.bash_profile" ]; then
+  install_profile "${HOME}/.bash_profile"
+elif [ -s "${HOME}/.profile" ]; then
+  install_profile "${HOME}/.profile"
+fi
--- profile.sh
+++ profile.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Aliases
 alias dir='ls -l'
 alias less='less --raw'
 alias ls='ls -G'
