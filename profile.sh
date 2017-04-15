a5fb1feb6165bb7e0dfbb7086637d2a3e3bd67c6
Pete Taylor <peter.taylor@gmail.com>
2017-04-15 09:42:39 -0500

    Add shortcuts for tmux copy/paste

--- profile.sh
+++ profile.sh
@@ -5,6 +5,8 @@ alias dir='ls -l'
 alias dotpull='git -C ~/.dotfiles pull && ~/.dotfiles/install.sh && echo "Done!"'
 alias less='less --raw'
 alias ls='ls -G'
+alias tmpaste='tmux save-buffer -'
+alias tmcopy='tmux load-buffer -'
 
 # Project directory "cd" aliases
 #
