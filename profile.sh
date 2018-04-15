3633e1d0e69e2583e73d87ebadc898219f9bc5fd
Peter Taylor <peter.taylor@gmail.com>
2018-04-15 11:05:06 -0500

    Cleanup restic cache permissions

--- profile.sh
+++ profile.sh
@@ -27,6 +27,7 @@ source ~/.restic
 backup() {
   if [[ "${BACKUP_USE_SUDO}" == "1" ]]; then
     sudo -E restic backup "${BACKUP_TARGETS[@]}"
+    sudo chown -R $(id -u):$(id -g) ~/.cache/restic
   else
     restic backup "${BACKUP_TARGETS[@]}"
   fi
