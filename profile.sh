f00b46b68fa39b55fbeacb2f0963d00f6435c349
Peter Taylor <peter.taylor@gmail.com>
2018-04-15 10:29:17 -0500

    Add restic configuration

--- profile.sh
+++ profile.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 
+# Restic
+if [ ! -f ~/.restic ]; then
+  cat <<SHELL > ~/.restic
+# Backblaze B2 credentials
+#export B2_ACCOUNT_ID=backblaze-account-id
+#export B2_ACCOUNT_KEY=backblaze-account-key
+
+# Restic repository of the form "b2:<bucket-name>:<path>"
+#export RESTIC_REPOSITORY=restic-repository
+
+# Load restic password from a file. Optional, will be prompted otherwise.
+#export RESTIC_PASSWORD_FILE=password-file
+
+# Files/directories to backup with "backup" command
+export RESTIC_TARGETS=( ~/Documents ~/Pictures )
+SHELL
+fi
+
+chmod 600 ~/.restic
+source ~/.restic
+
 # Aliases
+alias backup="restic backup ${RESTIC_TARGETS[@]}"
 alias dir='ls -l'
 alias dotpull='git -C ~/.dotfiles pull && ~/.dotfiles/install.sh && echo "Done!"'
 alias less='less --raw'
